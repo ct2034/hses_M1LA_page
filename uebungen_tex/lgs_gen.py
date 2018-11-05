2f88ac0481ac7b9707f30933a0e68027772e5ceb
ct2034 <post@henkelchristian.de>
2018-11-05 17:35:59 +0100

    uebungsblatt

---
+++ uebungen_tex/lgs_gen.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+SIZEA = 4
+SIZEB = 4
+
+def rand_nice_numbers(a, b=1):
+    return np.round(np.random.rand(a, b) * 10 - 5)
+
+def goal_check(A, x):
+    b = np.dot(A, x.T)
+    if(
+        (np.max(b) <= 3)
+        & (np.min(b) >= 0)
+    ):
+        return b
+    else:
+        return np.array([])
+
+if __name__ == "__main__":
+    b = np.array([])
+    while not b.any():
+        A = rand_nice_numbers(SIZEA, SIZEB)
+        x = rand_nice_numbers(1, SIZEB)
+        b = goal_check(A, x)
+    print(A)
+    print(x)
+    print(b)
